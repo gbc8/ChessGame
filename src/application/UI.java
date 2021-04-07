1d07b702006185eccb44f66497f2bc4ca4061159
Gabriel <gbc7@cin.ufpe.br>
2021-04-07 09:48:18 -0300

    Handiling captured pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -25,8 +28,9 @@ public class UI {
 		}
 	}
 	
-	public static void printMatch(ChessMatch chessMatch) {
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
+		printCapturedPieces(captured);
 		System.out.println("\nTurn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 	}
@@ -67,4 +71,16 @@ public class UI {
         }
         System.out.print(" ");
     }
+	
+	private static void printCapturedPieces(List<ChessPiece> captured) {
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println();
+		System.out.println("Captured pieces:");
+		System.out.print("White: ");
+		System.out.print(Arrays.toString(white.toArray()));
+		System.out.print("\nBlack: ");
+		System.out.print(Arrays.toString(black.toArray()));
+		System.out.println();
+	}
 }
